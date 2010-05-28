cf25620d46ffd5df82a5f402dfdd18663899a727
Anurag Priyam <anurag08priyam@gmail.com>
2010-05-28 20:51:16 +0530

    Parse trees as otus is parsed. Naturally it is not correct. Switching schema like class

--- lib/bio/db/nexml/elements.rb
+++ lib/bio/db/nexml/elements.rb
@@ -23,6 +23,31 @@ module Bio
       end
     end #end class otu
 
-  end
+    class Trees
+      attr_accessor :id, :label
 
-end
+      def initialize( id, label = nil )
+        @id = id
+        @label = label
+      end
+
+      def tree
+        @tree ||= []
+      end
+
+      def otus
+      end
+    end
+
+    class Tree
+      attr_accessor :id, :label
+
+      def initialize( id, label = nil )
+        @id = id
+        @label = label
+      end
+    end
+
+  end #end module NeXML
+
+end #end module Bio
