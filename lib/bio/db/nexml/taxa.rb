2d7223750936d4bdfb43e126efefd2cd7faaf074
Anurag Priyam <anurag08priyam@gmail.com>
2010-08-11 04:17:43 +0530

    Update taxa.rb to use Mapper.

--- lib/bio/db/nexml/taxa.rb
+++ lib/bio/db/nexml/taxa.rb
@@ -4,21 +4,28 @@ module Bio
     # Otu represents a taxon; an implementation of the
     # <em>Taxon</em>[http://nexml.org/nexml/html/doc/schema-1/taxa/taxa/#Taxon] type.
     # An Otu must have an 'id' and may take an an optional 'label'.
-    #    >> taxon1 = Bio::NeXML::Otu.new( 'taxon1', 'Label for taxon1' )
-    #    >> taxon1.id
-    #    => 'taxon1'
-    #    >> taxon1.label
-    #    => 'Label for taxon1'
+    #    taxon1 = Bio::NeXML::Otu.new( 'taxon1', :label => 'Label for taxon1' )
+    #    taxon1.id    #=> 'taxon1'
+    #    taxon1.label #=> 'Label for taxon1'
+    #    taxon1.otus  #=> otus object they belong to; see docs for Otus
     class Otu
       include Mapper
 
-      property  :id
-      property  :label
+      # A file level unique identifier.
+      attr_accessor  :id
 
+      # A human readable description.
+      attr_accessor  :label
+
+      # An otu is contained in otus.
       belongs_to :otus
 
+      # An otu is referred to by several tree nodes.
       has_n     :nodes
 
+      # Create a new otu.
+      #    otu = Bio::NeXML::Otu.id( 'o1' )
+      #    otu = Bio::NeXML::Otu.id( 'o1', :label => 'A label' )
       def initialize( id, options = {}, &block )
         @id = id
         properties( options ) unless options.empty?
@@ -33,30 +40,28 @@ module Bio
     # An Otus must have an 'id' and may take an optional 'label'. Adding two or more Otu objects
     # with the same 'id' to an Otus is not allowed. Doing so will overwrite the previous Otu object
     # with the same the same 'id'.
-    #    >> taxa1 = Bio::NeXML::Otus.new( 'taxa1', 'Label for taxa1' )
-    #    >> taxa1.id
-    #    => 'taxa1'
-    #    >> taxa1.label
-    #    => 'Label for taxa1'
+    #    taxa1 = Bio::NeXML::Otus.new( 'taxa1', :label => 'Label for taxa1' )
+    #    taxa1.id       #=> 'taxa1'
+    #    taxa1.label    #=> 'Label for taxa1'
     #
-    #    >> taxon1 = Bio::NeXML::Otu.new( 'taxon1', 'Label for taxon1' )
-    #    >> taxon2 = Bio::NeXML::Otu.new( 'taxon2', 'Label for taxon2' )
+    #    taxon1 = Bio::NeXML::Otu.new( 'taxon1' )
+    #    taxon2 = Bio::NeXML::Otu.new( 'taxon2' )
     #
-    #    >> taxa1 << taxon1 << taxon2
-    #    >> taxa1.count
-    #    => 2
-    #    >> taxa1.each { |otu| puts otu.id }
-    #    >> taxa1.include?( 'taxon1' )
-    #    => true
-    #    >> taxa1.delete( 'taxon2' )
-    #    >> taxa1.include?( taxon2 )
-    #    => false
+    #    taxa1 << taxon1 << taxon2
+    #    taxa1.count                      #=> 2
+    #    taxa1.each { |otu| puts otu.id }
+    #    taxon2.otus                      #=> taxa1
+    #    taxa1.include?( taxon1 )         #=> true
+    #    taxa1.delete( taxon2 )           #=> taxon2
     class Otus
       include Enumerable
       include Mapper
 
-      property :id
-      property :label
+      # A file level unique identifier.
+      attr_accessor :id
+
+      # A human readable description.
+      attr_accessor :label
 
       has_n :otus
       has_n :trees, :singularize => false
