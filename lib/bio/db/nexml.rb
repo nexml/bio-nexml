25a3260dd619ca3e6e5449ec1ece2386ae119a12
Rutger Vos <rutgeraldo@gmail.com>
2011-02-03 14:47:58 +0900

    The NexmlWritable superclass has been removed. The idea was to use it as a parent of biological data objects that need to be written to xml, providing them with the utility methods for creating nodes and processing xml attributes. This approach wasn't going to work because a number of the data objects need to inherit from core bioruby (Bio::* star classes). Hence, this was removed. In addition, I added a to_xml method that serializes a nexml root object and its children.

--- lib/bio/db/nexml.rb
+++ lib/bio/db/nexml.rb
@@ -28,77 +28,9 @@ module Bio
 
     autoload :Parser, 'bio/db/nexml/parser'
     autoload :Writer, 'bio/db/nexml/writer'
-    
-    class NexmlWritable < Writer
-
-      # Add helper functions to XML::Node class.
-      XML::Node.class_eval do
-  
-        # Assign namespaces to <tt>self</tt>.
-        # ---
-        # *Arguments*:
-        # * namespaces - a hash of prefix, uri pairs. It delegates the actual addition
-        # to the <tt>namespace=</tt> method.
-        # >> node = XML::Node.new( 'nexml' )
-        # >> node.namespaces = { :nex => "http://www.nexml.org/1.0" }
-        # >> node.namespaces = { nil => "http://www.nexml.org/1.0" }
-        # >> node
-        # => <nexml xmlns:nex="http://www.nexml.org/1.0" xmlns="http://www.nexml.org/1.0"/>
-        def namespaces=( namespaces )
-          namespaces.each do |prefix, prefix_uri|
-            self.namespace = prefix, prefix_uri
-          end
-        end
-  
-        # Assign attributes to <tt>self</tt>.
-        # ---
-        # *Arguments*:
-        # * attributes - a hash of name, value pairs. It delegates the actual addition
-        # to the <tt>attribute=</tt> method.
-        # >> node = XML::Node.new( 'nexml' )
-        # >> node.attributes = { :version => '0.9' }
-        # >> node
-        # => <nexml version="0.9"/>
-        def attributes=( attributes )
-          attributes.each do |name, value|
-            self.attribute = name, value
-          end
-        end
-  
-        # Assign a single attribte to <tt>self</tt>.
-        # ---
-        # *Arguments*:
-        # * pair - an array whose first value is the attribute's name and
-        # the second value is the attribute's value.
-        # >> node = XML::Node.new( 'nexml' )
-        # >> node.attribute = 'version', '0.9'
-        # >> node
-        # => <nexml version="0.9"/>
-        def attribute=( pair )
-          XML::Attr.new( self, pair.first.to_s, pair.last )
-        end
-  
-        # Assing a single namespace to <tt>self</tt>.
-        # ---
-        # *Arguments*:
-        # * pair - an array whose first value is the namespace prefix and
-        # the second value is the namespace uri. Use <tt>nil</tt> as a prefix
-        # to create a default namespace.
-        # >> node = XML::Node.new( 'nexml' )
-        # >> node.namespace = 'nex', "http://www.nexml.org/1.0"
-        # >> node.namespace = nil, 'http://www.nexml.org/1.0'
-        # >> node
-        # => <nexml xmlns:nex="http://www.nexml.org/1.0" xmlns="http://www.nexml.org/1.0"/>
-        def namespace=( pair )
-          # have to check for a nil prefix
-          prefix = ( p = pair.first ) ? p.to_s : p
-          XML::Namespace.new( self, prefix, pair.last )
-        end
-  
-      end
-    end
 
     class Nexml
+      @@writer = Bio::NeXML::Writer.new
       include Mapper
       include Enumerable
       attr_accessor :version
@@ -108,7 +40,7 @@ module Bio
       has_n :trees,      :singularize => false
       has_n :characters, :singularize => false
       
-      def initialize( version, generator = 'bioruby' )
+      def initialize( version = '0.9', generator = 'bioruby' )
         @version = version
         @generator = generator
       end
@@ -125,6 +57,22 @@ module Bio
           add_characters element
         end
       end
+      
+      def to_xml
+        node = @@writer.create_node( "nexml", :"xsi:schemaLocation" => "http://www.nexml.org/2009 http://www.nexml.org/2009/xsd/nexml.xsd", :generator => generator, :version => version )
+        node.namespaces = { nil => "http://www.nexml.org/2009", :xsi => "http://www.w3.org/2001/XMLSchema-instance", :xlink => "http://www.w3.org/1999/xlink", :nex => "http://www.nexml.org/2009" }
+        self.each_otus do |otus|
+            node << otus.to_xml
+        end
+        self.each_characters do |characters|
+            node << characters.to_xml
+        end
+        self.each_trees do |trees|
+            node << trees.to_xml
+        end        
+        node
+      end      
+      
     end #end class Nexml
   end
 end
